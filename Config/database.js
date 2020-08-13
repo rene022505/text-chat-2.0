1595f67b3a7966fad42912f8a8b03357de7018e6
SimplyRon <renaudgdumont@gmail.com>
2020-08-13 19:46:25 -0400

    Replace MySQL dependancy by MySQL2 Add Sequelize Add Database name to env file

--- Config/database.js
+++ Config/database.js
@@ -5,10 +5,14 @@ const envFile = "./Config/config.env";
 
 dotenv.config({path: envFile});
 
-const con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+const sequelize = new Sequelize(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql'
 });
+// const con = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD
+// });
 
 module.exports = con;
